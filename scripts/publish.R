9e324a2ee796c3513022464c6c5386cf6be94cf2
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-08-04 09:45:01 -0300

    Altera variaveis de ambiente para ambiente de desenvolvimento

--- scripts/publish.R
+++ scripts/publish.R
@@ -5,7 +5,7 @@ id <- jsonlite::read_json("datapackage.json")$resources[[1]]$id
 
 res <- ckanr::resource_update(id = id,
                        path = output, 
-                       url = Sys.getenv("DADOSMG_PROD_HOST"), 
-                       key = Sys.getenv("DADOSMG_PROD"))
+                       url = Sys.getenv("DADOSMG_DEV_HOST"), 
+                       key = Sys.getenv("DADOSMG_DEV"))
 
 stopifnot(check_upload(file = output, url = res$url))
