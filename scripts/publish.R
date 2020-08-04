029ab38a358f51e1dfb631b114ef43f92971b543
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2020-08-04 09:43:57 -0300

    Remove dependencia python

--- scripts/publish.R
+++ scripts/publish.R
@@ -1,15 +1,10 @@
-library(reticulate)
-
 source("scripts/lib/utils.R")
-
-use_virtualenv("renv/python/virtualenvs/renv-python-3.7.3", required = TRUE)
-source_python("scripts/lib/utils.py")
 
 output <- jsonlite::read_json("datapackage.json")$resources[[1]]$path
 id <- jsonlite::read_json("datapackage.json")$resources[[1]]$id
 
-res <- upload_resource(output, 
-                       resource_id = id, 
+res <- ckanr::resource_update(id = id,
+                       path = output, 
                        url = Sys.getenv("DADOSMG_PROD_HOST"), 
                        key = Sys.getenv("DADOSMG_PROD"))
 
